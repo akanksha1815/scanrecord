9aeb7c85d2178d9ca90d0e317ea1c0cd3d51302a
Akanksha Priyadarshini <akanksha96ca@gmail.com>
2025-08-03 18:44:12 -0400

    updated app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg' // served from public folder
 import './App.css'
 
 function App() {
-const viteLogo = '/vite.svg'; 
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
